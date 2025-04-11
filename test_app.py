c6e4b586606ee9e095a735ff51c77bc77cde2565
Joana De Luna <163531588+joanamariebdeluna@users.noreply.github.com>
2025-04-11 09:46:13 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import miles_to_kilometers
 
 def test_miles_to_kilometers():
     assert miles_to_kilometers(1) == 1.60934
-    assert miles_to_kilometers(5) == 8.0467
+    assert miles_to_kilometers(5) == 8.04672
